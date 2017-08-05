0a761df4f2f8ff7b4274a03d23f2f93f525f7310
guoweifeng216 <782388110@qq.com>
2017-08-05 16:37:30 +0800

    add note information

---
+++ Note/git_user.py
@@ -0,0 +1,22 @@
+git常用命令:
+1.git add file(git add *)添加文件
+2.git rm file
+3.git commit -m "msg"
+  git commit -a -m "msg" 直接提交
+4.git status 查看当前状态
+5.git log 查看版本信息
+6.ls -a 显示出所有文件包括隐藏文件
+ 蓝色文件夹
+ 白色文件
+7.git show 版本号
+8.git pull
+9.git push 在commit后
+10 git remote add 名称 https://github.com/guoweifeng216/
+11 git push 名称 master（branch）
+12 git clone https
+13 git diff
+
+正常上传文件到GitHub过程：
+1.git add file
+2 git commit -m "msg"
+3.git push 名称 master

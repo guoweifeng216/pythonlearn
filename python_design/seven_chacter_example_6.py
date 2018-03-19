fc4d12949f4bf26935e58ff4d2c30e9a602a77ad
wfguo <weifeng.guo@cnexlabs.com>
2018-03-19 09:23:28 +0800

    modify some case

--- python_design/seven_chacter_example_6.py
+++ python_design/seven_chacter_example_6.py
@@ -13,9 +13,11 @@ def main():
         listOfStudent.append(st)
         carryOn=raw_input("Do you want to continue(Y/N)?")
         carryOn=carryOn.upper()
+    print listOfStudent
+    print type(listOfStudent)
     print "\nNAME\tGRADE"
-    listOfStudent=listOfStudent.sort()
+    listOfStudent.sort(key=lambda x:x.getName())
     for pupil in listOfStudent:
         print pupil
 main()
-cij_runner --hook-pr-trun dmesg sysinf pblk --env $CIJ_ENVS/hz-m2-fb3-131-48.sh --testsuite /root/git/cijoe/testsuites/block_smoke_01.suite -vv
+# ['as	B']
--- python_design/student_term.py
+++ python_design/student_term.py
@@ -11,15 +11,17 @@ def obtainListOfStudent():
     listOfStudent=[]
     while carryOn=='Y':
         name=raw_input("Enter student's name:")
-        midterm=raw_input("Enter student's grade on midterm exam:")
-        final=raw_input("Enter student's grade on final exam")
+        midterm=float(raw_input("Enter student's grade on midterm exam:"))
+        final=float(raw_input("Enter student's grade on final exam"))
         category=raw_input("Enter caregory (LG or PF):")
         if category=='LG':
             st=seven_2_1.LGstudent(name,midterm,final)
+            ST1=st.calcSemGrade()
         else:
             st=seven_2_1.PFstudent(name,midterm,final)
+            ST1=st.calcSemGrade()
 
-
+        print
         listOfStudent.append(st)
         carryOn=raw_input("Do you want to continue (Y/N)?")
         carryOn=carryOn.upper()
@@ -27,6 +29,7 @@ def obtainListOfStudent():
     return listOfStudent
 
 def displayResults(listOfStudent):
+    print listOfStudent
     print "\nNAME\tGRADE"
     listOfStudent.sort(key=lambda x:x.getName())
     for pupil in listOfStudent:
